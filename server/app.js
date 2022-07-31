25563177b3841c9d4532ef7ecdb62cca2d960397
venkateshellaboina <venkateshellaboina@gmail.com>
2022-07-31 13:32:18 -0400

    Feature: Moved strings to a constant file

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import getVacantSlipNumber from './utils/util.js';
 import lowdbInit from './database/setup.js';
+import { NO_AVAILABLE_BOAT_SLIP, BOAT_SLIP_VACANT, URI_GET_BOATSLIPS, URI_POST_BOATSLIPS, URI_PUT_BOATSLIP } from './constants/constants.js'
 
 // set up express app
 const app = express();
@@ -10,18 +11,18 @@ app.use(express.json());
 
 var lowdb = await lowdbInit()
 
-app.get("/boat-slips", (req, res) => {
+app.get(URI_GET_BOATSLIPS, (req, res) => {
     res.status(200).json(lowdb.data.boatSlips)
     return
 })
 
-app.post("/boat-slips", async (req, res) => {
+app.post(URI_POST_BOATSLIPS, async (req, res) => {
     const {body: {vesselName}} = req
     let boatSlips = lowdb.data.boatSlips
     let slipNumber = getVacantSlipNumber(boatSlips)
 
     if(slipNumber == -1){
-        res.status(409).json({Message: "There are no available boat slips."})
+        res.status(409).json({statusCode: 409, Message: NO_AVAILABLE_BOAT_SLIP})
         return
     }
     
@@ -30,20 +31,19 @@ app.post("/boat-slips", async (req, res) => {
 
     await lowdb.write()
 
-    res.json({slipNumber: slipNumber})
+    res.status(200).json({slipNumber: slipNumber})
     return
-
 })
 
 
 
-app.put("/boat-slips/:slipNumber/vacate", async (req, res) => {
+app.put(URI_PUT_BOATSLIP, async (req, res) => {
     let {params: {slipNumber}} = req
     let isVacant = lowdb.data.boatSlips[slipNumber-1].vacant
 
     if(isVacant){
-        let isVacantError = "Boat slip '" + slipNumber + "' is currently vacant"
-        res.status(409).json({Message: isVacantError})
+        let isVacantError = BOAT_SLIP_VACANT.replace('x', slipNumber) 
+        res.status(409).json({statusCode: 409, Message: isVacantError})
         return
     }
 
---
+++ server/constants/constants.js
@@ -0,0 +1,10 @@
+//string constants
+export const NO_AVAILABLE_BOAT_SLIP = "There are no available boat slips."
+export const BOAT_SLIP_VACANT = "Boat slip 'x' is currently vacant."
+
+//URI constants
+export const URI_GET_BOATSLIPS = "/boat-slips"
+export const URI_POST_BOATSLIPS = "/boat-slips"
+export const URI_PUT_BOATSLIP = "/boat-slips/:slipNumber/vacate"
+
+
